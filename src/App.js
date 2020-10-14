a5d4a268979df806f8b3427059b73b4f846b012c
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-10-14 18:53:11 +0530

    added complete app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Home from "./routes/Home";
+import Registry from "./routes/Registry";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Registry App
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+
+          <Route path='/registry'>
+            <Registry />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
