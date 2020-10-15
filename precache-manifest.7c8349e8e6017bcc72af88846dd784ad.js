4999bb1b5c2c32c7a3b54a533042c0b5a106e0a5
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-10-15 08:23:46 +0530

    Updates

---
+++ precache-manifest.7c8349e8e6017bcc72af88846dd784ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66833e6f25c4bc953d92539a5fdf0d71",
+    "url": "/registry-app/index.html"
+  },
+  {
+    "revision": "432c3814e890d1b241cf",
+    "url": "/registry-app/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "6c9d44f5c591b258a951",
+    "url": "/registry-app/static/js/2.bca26d28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/registry-app/static/js/2.bca26d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432c3814e890d1b241cf",
+    "url": "/registry-app/static/js/main.52dbfb17.chunk.js"
+  },
+  {
+    "revision": "45f10bbac9364d7c5dc9",
+    "url": "/registry-app/static/js/runtime-main.da4c825c.js"
+  }
+]);
